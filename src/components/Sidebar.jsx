ee533b0d5821f842a8d11e1adf72af7d9bec74ad
Pedro Miguel <pedromiguelboce@gmail.com>
2024-10-14 18:22:03 -0300

    feat(avatar): adicionar componente Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import {PencilLine} from 'phosphor-react';
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -8,7 +9,7 @@ export function Sidebar() {
             src='https://images.unsplash.com/photo-1508739826987-b79cd8b7da12?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
             
             <div className={styles.profile}>
-                <img className={styles.avatar} src='https://github.com/pedrocbp.png' />
+                <Avatar src='https://github.com/pedrocbp.png'/>
                 <strong>Pedro Miguel</strong>
                 <span>Full Stack Developer</span>
             </div>
