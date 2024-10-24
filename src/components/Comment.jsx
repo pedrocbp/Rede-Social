420e8ec4d53798c6cdfc2e5bfe7fcba8eb654336
Pedro Miguel <pedromiguelboce@gmail.com>
2024-10-24 00:29:31 -0300

    feat: adicionar botao de curtir um comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({content, onDeleteComment}) {
 
+    const [likeCount, setLikeCount] = useState(0);
+
     function handleDeleteComment() {
         onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1);
     }
 
     return (
@@ -28,9 +35,9 @@ export function Comment({content, onDeleteComment}) {
                 </div>
 
                 <footer>
-                    <button >
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                         <p>Curtir</p><span>20</span>
+                         <p>Curtir</p><span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
